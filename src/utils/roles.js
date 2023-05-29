8f241f5e50ea714c94619a8e83b4f73e986a3e67
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-05-29 18:39:21 -0400

    playerAction added to roles json

--- src/utils/roles.js
+++ src/utils/roles.js
@@ -5,6 +5,7 @@ export const characters = {
 		displayText:
 			"You are lowly villager, doomed to spend your days plowing a field.",
 		prompt: "These are the current players",
+		action: "DONOTHING",
 		team: 0,
 	},
 	seer: {
@@ -12,6 +13,7 @@ export const characters = {
 		displayText:
 			"You are the Seer. Each night, you may choose a player and have their role revealed to you. Admit to your role wisely! If you reveal you are the seer, the wolves will try to kill you!",
 		prompt: "Choose a player, and their role will be revealed to you",
+		action: "SEE",
 		team: 0,
 	},
 	guardianAngel: {
@@ -19,6 +21,7 @@ export const characters = {
 		displayText:
 			"You are the Guardian Angel! Each night, you may choose a player to watch over. Be careful however - if you choose to watch over a wolf, there is a 50% chance you will be killed, and if you watch over the serial killer, you will be killed.",
 		prompt: "Choose a player to protect tonight",
+		action: "SAVE",
 		team: 0,
 	},
 	werewolf: {
@@ -26,6 +29,7 @@ export const characters = {
 		displayText:
 			"Each night, you will get to pick a player to kill. Make sure your fellow werewolves agree on the same player to kill!",
 		prompt: "Vote a player to feed on tonight",
+		action: "EAT",
 		team: 1,
 	},
 };
